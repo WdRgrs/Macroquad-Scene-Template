446d1a0e43bc651621367d0736dfbdc13ca64105
Wade Rogers <wademac@Wades-MacBook-Pro.local>
2025-05-29 15:33:33 -0500

    updated missed project-name on toml, adjust itchy script

--- scripts/itch.sh
+++ scripts/itch.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# TODO Ensure project-name works - fallback to swap to
-# dir_name=$(basename "$PWD")
-
 # Check and remove existing web dir - Probably unnecessary - fresh start
 if [ -d "./web" ]; then
   echo "Removing old web dir..."
-  rm -rf ./web/*
+  rm -rf ./web
 fi
+mkdir ./web
 
-echo "$dir_name"
 # Install WebAssembly
 echo "Ensure WASM installed..."
 rustup target add wasm32-unknown-unknown
@@ -18,35 +15,28 @@ rustup target add wasm32-unknown-unknown
 echo "Building WebAssembly binary..."
 cargo build --release --target wasm32-unknown-unknown
 
-
 # Copy the wasm file to the root directory
 echo "Copying WASM file to root..."
-mkdir ./web
-# TODO Ensure project-name works - fallback to swap to
 cp target/wasm32-unknown-unknown/release/{{project-name}}.wasm ./web
-# cp target/wasm32-unknown-unknown/release/"$dir_name".wasm ./web
 
 # at this time, itch.io is not playing nice with sub-directories under 'assets' - assets work best at root =(
 echo "Copying assets file to root..."
-cp -r assets/sounds/* ./web
-cp -r assets/fonts/* ./web
+if [ -d "assets/sounds" ]; then
+  echo "Found sounds"
+  cp -r assets/sounds/* ./web
+elif [ -d "assets/fonts" ]; then
+  echo "Found fonts"
+  cp -r assets/fonts/* ./web
+fi
 
 echo "Copying index.html file to root..."
 cp index.html ./web
 
-
 echo "Zippity zop"
 if [[ "$OSTYPE" == darwin* ]]; then
   echo "Mac-ing"
-  # TODO Ensure project-name works - fallback to swap to
   zip -r "web_{{project-name}}.zip" web/*
-fi
-
-if [[ "$OSTYPE" == msys* ]]; then
+elif [[ "$OSTYPE" == msys* ]]; then
   echo "Win-dos"
-  # TODO Ensure project-name works - fallback to swap to
   powershell Compress-Archive -Path "web\*" -DestinationPath "web_{{project-name}}.zip" -Force
-
 fi
-# windows
-# mac
